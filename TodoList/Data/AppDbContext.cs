b2c7b5cd5d84acf72ad194dadb2013478444e433
Ahmed0484 <a_elshazly_08@hotmail.com>
2024-09-25 20:36:06 +0300

    Add project files.

---
+++ TodoList/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoList.Models;
+
+namespace TodoList.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Mission> Missions { get; set; }
+    }
+}
---
+++ TodoList/Models/Mission.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoList.Models
+{
+    public class Mission
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }=string.Empty;
+        public bool Done { get; set; } = false;
+    }
+}
